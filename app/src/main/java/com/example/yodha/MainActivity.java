81935b8f869eb56d51f7f07bc71e3a81ba8be5bc
Avanish474 <60430980+Avanish474@users.noreply.github.com>
2020-09-29 23:00:30 +0530

    Add files via upload

--- app/src/main/java/com/example/yodha/MainActivity.java
+++ app/src/main/java/com/example/yodha/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.yodha;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        next = findViewById(R.id.button);
 
 
+    }
+
+    public void nextplease(View view) {
+        Intent intent = new Intent(this, QuizActivity.class);
+        startActivity(intent);
     }
 }
